b990a57450195fe57114ba8a40ef532d61738202
Nguyen <nguyen0111@users.noreply.github.com>
2026-04-01 14:22:56 -0400

    revert to original unformatted code

--- service/routes.py
+++ service/routes.py
@@ -40,9 +40,7 @@ def list_counters():
     """Lists all counters"""
     app.logger.info("Request to list all counters...")
 
-    counters = [
-        dict(name=count[0], counter=count[1]) for count in COUNTER.items()
-    ]
+    counters = [dict(name=count[0], counter=count[1]) for count in COUNTER.items()]
 
     return jsonify(counters)
 
@@ -56,9 +54,7 @@ def create_counters(name):
     app.logger.info("Request to Create counter: %s...", name)
 
     if name in COUNTER:
-        return abort(
-            status.HTTP_409_CONFLICT, f"Counter {name} already exists"
-        )
+        return abort(status.HTTP_409_CONFLICT, f"Counter {name} already exists")
 
     COUNTER[name] = 0
 
@@ -79,9 +75,7 @@ def read_counters(name):
     app.logger.info("Request to Read counter: %s...", name)
 
     if name not in COUNTER:
-        return abort(
-            status.HTTP_404_NOT_FOUND, f"Counter {name} does not exist"
-        )
+        return abort(status.HTTP_404_NOT_FOUND, f"Counter {name} does not exist")
 
     counter = COUNTER[name]
     return jsonify(name=name, counter=counter)
@@ -96,9 +90,7 @@ def update_counters(name):
     app.logger.info("Request to Update counter: %s...", name)
 
     if name not in COUNTER:
-        return abort(
-            status.HTTP_404_NOT_FOUND, f"Counter {name} does not exist"
-        )
+        return abort(status.HTTP_404_NOT_FOUND, f"Counter {name} does not exist")
 
     COUNTER[name] += 1
 
@@ -128,4 +120,3 @@ def reset_counters():
     global COUNTER  # pylint: disable=global-statement
     if app.testing:
         COUNTER = {}
-        
